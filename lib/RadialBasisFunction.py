7090c1ebac109a3ecf0f792cea68ae5a393a93d4
Mehmet <guel_mehmet@hotmail.de>
2021-06-23 14:59:10 +0200

    Put some helperfunctions into the utils.py and added some comments

--- lib/RadialBasisFunction.py
+++ lib/RadialBasisFunction.py
@@ -6,10 +6,16 @@ from numpy import linalg as LA
 class RBF:
 
     def __init__(self, initial_data, transformed_data, amount_of_centers, epsilon, delta_t, vector_field=True):
-        """
+         """
+        Constructor for the RBF class
 
-        :type initial_data: object
-        :type transformed_data: object
+        Parameters:
+            initial_data: The initial data
+            transformed_data: The data updated by an update function that we want to approximate
+            amount_of_centers: how many radial basis functions the approximated function should use
+            epsilon: how wide the radial basis functions are supposed to be
+            delta_t: the time it took to get from the initial_data to the transformed_data
+            vector_field: if true then the calculations are done for a vector field and the approximated function will give a velocity and not predicted points
         """
         if transformed_data.ndim == 1:
             self.transformed_data = transformed_data.reshape(-1, 1)
@@ -34,9 +40,30 @@ class RBF:
         self.C = C
 
     def get_approximate_velocity(self, x0, x1):
+        """
+        Returns a velocity vector for the two given sets of points x0 and x1
+
+        Parameters:
+            x0: Start position
+            x1: End position
+
+        Returns:
+            tuple: the velocity necessary for the point to reach its target in the time delta_t
+
+        """
         return (x1 - x0) / self.delta_t
 
     def rbf(self, to_predict):
+        """
+        uses the approximated function on the given points
+
+        Parameters:
+            to_predict: the points for which the approximated function is going to be calculated
+
+        Returns:
+            array of points: The outcome of the calculation
+
+        """
         distance = distance_matrix(
             to_predict, self.centers)
         theta = np.exp(-((distance) / (self.epsilon))**2)
--- lib/utils.py
+++ lib/utils.py
@@ -1,3 +1,5 @@
+from numpy import linalg as LA
+
 def shift_coordinates(x, m, delta_t):
     """
     shifts the values of x by k*delta_t, where k is 0, 1, 2, ..., m
@@ -16,3 +18,37 @@ def shift_coordinates(x, m, delta_t):
          for k in range(1, m + 1)]
     )
     return (x[:-m*delta_t],) + new_coordinates
+
+def get_v_from_data(x0,x1, t):
+    """
+    Returns a velocity vector for the two given sets of points x0 and x1
+
+    Parameters:
+        x0: Start position
+        x1: End position
+        t : set the time it took to reach the end position
+
+    Returns:
+        tuple: the velocity necessary for the point to reach its target in the given time
+
+    """
+    return (x1-x0)/t
+
+def mse(x0,x1):
+    """
+    Returns the mean square error for the given points
+
+    Parameters:
+        x0: Set of points
+        x1: Set of points
+
+    Returns:
+        float: mean square error of all the points
+
+    """
+    N = x0.shape[0]
+    s = 0
+    for i in range(N):
+        distance = LA.norm(x0[i]-x1[i],ord = 2)**2
+        s = s + distance
+    return s/N
